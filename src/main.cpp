30bbc420aaae9417187a910949225240895998fd
Jason Poindexter <poindexter.json@gmail.com>
2024-07-26 17:25:25 -0700

    Only look at Z access and use deviation

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ const char *uniqueHostname = uniqueHostnameStr.c_str();
 
 // General sensitivity of the step detection
 // Eg: 800 = more sensitive, 1600 = less sensitive
-#define STEP_SENSITIVITY 25.0
+#define STEP_SENSITIVITY 45.0
 #define AUDIO_VOLUME 30 // Volume for the audio player, adjust as needed 0 - 30
 #define SDA_PIN 19      // I2C SDA pin
 #define SCL_PIN 23      // I2C SCL pin
@@ -46,24 +46,47 @@ DY::Player player(&Serial2);
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 
 // Forward declarations
+// AUDIO
 void initAudio();
-
-void initAccel();
-
-void displaySensorDetails(void);
-
-bool stepDetected();
 
 void handleAudioPlayback();
 
-void accelPoll();
+// ACCEL
+// Filter and threshold parameters
+const int numReadings = 10;
+int readings[numReadings];      // Circular buffer for readings
+int readIndex = 0;              // Current index in buffer
+int total = 0;                  // Running total for average
+int averageZ = 0;               // Calculated average
+int dynamicThreshold = 600;     // Initial dynamic threshold
+
+void initAccel();
+
+void displaySensorDetails();
+
+int getCurrentZ();
+
+int calculateMovingAverage(int newReading);
+
+int calculateStandardDeviation();
+
+bool isStepDetected(int currentZ);
+
+void waitForStepReset(int currentZ);
+
+void initializeReadings();
+
+
+// BOARD
+void initBuiltInLED();
 
 void builtInLEDOn();
 
 void builtInLEDOff();
 
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    initBuiltInLED();
     builtInLEDOn();
 
     Serial.begin(115200);
@@ -82,22 +105,57 @@ void setup() {
     builtInLEDOff();
 }
 
+#define EVERY_N_MILLISECONDS(interval, lastTime, action) do { \
+    static unsigned long lastTime = millis(); \
+    unsigned long currentTime = millis(); \
+    if (currentTime - lastTime >= interval) { \
+        lastTime = currentTime; \
+        action; \
+    } \
+} while(0)
+
 void loop() {
     handleOTA();
-    accelPoll();
-    if (stepDetected()) {
-        builtInLEDOn();
-        handleAudioPlayback();
-        delay(350); // Sample rate control
-        builtInLEDOff();
-    }
-    // digitalWrite(LED_BUILTIN , HIGH);
+    EVERY_N_MILLISECONDS(10, lastTime1, {
+
+        int currentZ = getCurrentZ();
+        Serial.print("Current Z: ");
+        Serial.print(currentZ);
+
+        averageZ = calculateMovingAverage(currentZ);
+        Serial.print(" | Average Z: ");
+        Serial.print(averageZ);
+
+        int stdDeviation = calculateStandardDeviation();
+        Serial.print(" | Standard Deviation: ");
+        Serial.print(stdDeviation);
+
+
+        dynamicThreshold =
+                averageZ + stdDeviation;  // Setting threshold as one standard deviation above the mean
+        Serial.print(" | Dynamic Threshold: ");
+        Serial.print(dynamicThreshold);
+
+        if (isStepDetected(currentZ)) {
+            Serial.print(" | Step detected!");
+            builtInLEDOn();
+            handleAudioPlayback();
+            waitForStepReset(currentZ);
+            builtInLEDOff();
+        }
+        Serial.println();
+    });
 
 }
+
 
 ///////////
 // BOARD //
 ///////////
+void initBuiltInLED() {
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
 // NOTE: The onboard LED is active low.
 void builtInLEDOn() {
     digitalWrite(LED_BUILTIN, LOW);
@@ -124,36 +182,6 @@ void handleAudioPlayback() {
 ///////////
 // accel //
 ///////////
-int bufferSize();
-
-bool fillBuffer();
-
-bool equalReadings(AccelReading a, AccelReading b);
-
-AccelReading getCurrentReading();
-
-AccelReading getPreviousReading();
-
-double getMagnitude(AccelReading reading);
-
-double getVector(AccelReading reading);
-
-int getDelta();
-
-void printBuffer();
-
-void printDelta();
-
-void printMagnitude();
-
-void accelPoll();
-
-void calibrate();
-
-AccelReading accelBuffer[10]; // Buffer for storing the last 10 readings.
-int bufferPosition;           // Current read position of the buffer.
-
-double calibration; // Baseline for accelerometer data.
 
 void initAccel() {
     Wire.begin(SDA_PIN, SCL_PIN);
@@ -197,196 +225,51 @@ void initAccel() {
             break;
     }
 
-    bufferPosition = 0;
 
-    // Initialize the full buffer to zero.
-    for (int i = 0; i < bufferSize(); i++) {
-        accelBuffer[i].x = 0;
-        accelBuffer[i].y = 0;
-        accelBuffer[i].z = 0;
-    }
-
-    calibrate();
+    // Initialize the readings array
+    initializeReadings();
 }
 
-void calibrate() {
-
-    Serial.println("Calibrating accelerometer...");
-    calibration = 0;
-
-    while (1) {
-        // Fill the buffer.
-        if (!fillBuffer()) {
-            delay(10);
-            continue;
-        }
-        Serial.println("Passed fill buffer");
-
-        // Check to see if we're done.
-        bool pass = true;
-        double avg = 0;
-        for (int i = 0; i < bufferSize(); i++) {
-            double m = getMagnitude(accelBuffer[i]);
-            pass = pass && (abs(m - calibration) < 10);
-            avg += m;
-        }
-
-        if (pass) {
-            Serial.println("Calibration complete.");
-            break;
-        } else {
-            avg /= bufferSize();
-            calibration = avg;
-        }
+void initializeReadings() {
+    for (int &reading: readings) {
+        reading = 0;
     }
 }
 
-// Gathers data from accelerometer into the buffer. Only writes to the buffer
-// if the hardware has gathered data since we last wrote to the buffer.
-void accelPoll() {
-    // Read new accelerometer data. If there is no new data, return immediately.
-    if (!fillBuffer()) {
-        return;
-    }
-
-    /* PRINT DATA: */
-    // printBuffer();
-    // printDelta();
-    // printMagnitude();
-    // Serial.println();
-}
-
-// Gets the vector for the given reading.
-double getVector(AccelReading reading) {
-    double normalizedVector = abs(calibration - getMagnitude(reading));
-    return normalizedVector;
-}
-
-///////////////////////////////////////////////////////////////////
-
-// This may or may not fill the next buffer position. If the accelerometer hasn't
-// processed a new reading since the last buffer, this function immediately exits,
-// returning false.
-// Otherwise, if the accelerometer has read new data, this function advances the
-// buffer position, fills the buffer with accelerometer data, and returns true.
-bool fillBuffer() {
-    // Read from the hardware.
+int getCurrentZ() {
     sensors_event_t event;
     accel.getEvent(&event);
+    return event.acceleration.z;
+}
 
-    AccelReading newReading;
-    newReading.x = event.acceleration.x;
-    newReading.y = event.acceleration.y;
-    newReading.z = event.acceleration.z;
+int calculateMovingAverage(int newReading) {
+    total = total - readings[readIndex];
+    readings[readIndex] = newReading;
+    total = total + readings[readIndex];
+    readIndex = (readIndex + 1) % numReadings;
+    return total / numReadings;
+}
 
-    // Serial.printf("New Reading: %f, %f, %f\n", newReading.x, newReading.y, newReading.z);
-
-    // The accelerometer hasn't processed a new reading since the last buffer.
-    // Do nothing and return false.
-    if (equalReadings(getCurrentReading(), newReading)) {
-        // Serial.println("No new data.");
-        return false;
+int calculateStandardDeviation() {
+    int mean = averageZ;
+    int sumDeviation = 0;
+    for (int reading: readings) {
+        sumDeviation += pow(reading - mean, 2);
     }
+    return sqrt(sumDeviation / numReadings);
+}
 
-    // The accelerometer has read new data.
+bool isStepDetected(int currentZ) {
+    return currentZ > dynamicThreshold;
+}
 
-    // Advance the buffer.
-    if (++bufferPosition >= bufferSize()) {
-        bufferPosition = 0;
+void waitForStepReset(int currentZ) {
+    while (currentZ > averageZ) {
+        currentZ = getCurrentZ();  // Update currentZ with the latest Z-axis value
     }
-
-    AccelReading *mutableCurrentReading = &accelBuffer[bufferPosition];
-
-    mutableCurrentReading->x = newReading.x;
-    mutableCurrentReading->y = newReading.y;
-    mutableCurrentReading->z = newReading.z;
-
-    return true;
 }
 
-///////////////////////////////////////////////////////////////////
-
-// Gets the average difference between the latest buffer and previous buffer.
-int getDelta() {
-    AccelReading previousReading = getPreviousReading();
-    AccelReading currentReading = getCurrentReading();
-
-    int deltaX = abs(abs(currentReading.x) - abs(previousReading.x));
-    int deltaY = abs(abs(currentReading.y) - abs(previousReading.y));
-    int deltaZ = abs(abs(currentReading.z) - abs(previousReading.z));
-
-    return (deltaX + deltaY + deltaZ) / 3;
-}
-
-void printDelta() {
-    AccelReading previousReading = getPreviousReading();
-    AccelReading currentReading = getCurrentReading();
-
-    int deltaX = abs(abs(currentReading.x) - abs(previousReading.x));
-    int deltaY = abs(abs(currentReading.y) - abs(previousReading.y));
-    int deltaZ = abs(abs(currentReading.z) - abs(previousReading.z));
-
-    Serial.print(deltaX);
-    Serial.print("\t");
-    Serial.print(deltaY);
-    Serial.print("\t");
-    Serial.print(deltaZ);
-    Serial.print("\t");
-    Serial.print(getDelta());
-    Serial.println();
-}
-
-// Gets the vector magnitude for the given reading.
-// http://en.wikipedia.org/wiki/Euclidean_vector#Length
-double getMagnitude(AccelReading reading) {
-    double x = reading.x;
-    double y = reading.y;
-    double z = reading.z;
-
-    double vector = x * x + y * y + z * z;
-
-    return sqrt(vector);
-}
-
-void printMagnitude() {
-    Serial.println(getMagnitude(getCurrentReading()));
-}
-
-// Prints the latest buffer reading to the screen.
-void printBuffer() {
-    Serial.print(accelBuffer[bufferPosition].x);
-    Serial.print("\t");
-    Serial.print(accelBuffer[bufferPosition].y);
-    Serial.print("\t");
-    Serial.print(accelBuffer[bufferPosition].z);
-    Serial.println();
-}
-
-///////////////////////////////////////////////////////////////////
-
-// Returns the number of items held by the buffer.
-int bufferSize() {
-    return sizeof(accelBuffer) / sizeof(accelBuffer[0]);
-}
-
-AccelReading getCurrentReading() {
-    return accelBuffer[bufferPosition];
-}
-
-// Gets the previous buffer reading.
-AccelReading getPreviousReading() {
-    int previous = bufferPosition - 1;
-    if (previous < 0)
-        previous = bufferSize() - 1;
-    return accelBuffer[previous];
-}
-
-// Returns true if two readings are equal.
-bool equalReadings(AccelReading a, AccelReading b) {
-    return a.x == b.x && a.y == b.y && a.z == b.z;
-}
-
-void displaySensorDetails(void) {
+void displaySensorDetails() {
     sensor_t sensor;
     accel.getSensor(&sensor);
     Serial.println("------------------------------------");
@@ -408,16 +291,5 @@ void displaySensorDetails(void) {
     Serial.println("------------------------------------");
     Serial.println("");
     delay(500);
-}
-
-bool stepDetected() {
-    double upperBound = STEP_SENSITIVITY;
-    AccelReading currentReading = getCurrentReading();
-//    Serial.printf("Current Reading: %f, %f, %f\n", currentReading.x, currentReading.y, currentReading.z);
-    double normalizedVector = abs(calibration - getMagnitude(currentReading));
-    double scale = normalizedVector / upperBound;
-//    Serial.printf("Normalized Vector: %f\n", normalizedVector);
-//    Serial.printf("Scale: %f\n", scale);
-    return scale >= 1;
 }
 
